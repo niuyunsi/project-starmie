2d448550d2ef45d6f7402960044fded346b4f3ac
Yunsi Niu <niuyunsi@gmail.com>
2025-01-22 14:50:22 +0800

    feat: add Wish page with textarea input and layout, include navigation link from Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -47,6 +48,18 @@ export default function Home() {
       </a>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <Link
+          href="/wish"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Wish{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </Link>
+
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
--- app/wish/page.tsx
+++ app/wish/page.tsx
@@ -1,17 +1,35 @@
 "use client";
 
+import * as React from "react";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function WishPage() {
-  const handleClick = () => {
-    console.log("click");
+  const [textareaValue, setTextareaValue] = React.useState("");
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(e.target.value);
+    setTextareaValue(e.target.value);
+  };
+
+  const [output, setOutput] = React.useState("");
+  const handleButtonClick = () => {
+    console.log("handleButtonClick");
+    setOutput(textareaValue);
   };
 
   return (
-    <div>
+    <div className="p-4 flex flex-col items-start space-y-4 bg-gray-100">
       <h1>Wish Page</h1>
 
-      <Button onClick={handleClick}>Click me</Button>
+      <Textarea
+        placeholder="Paste url here"
+        value={textareaValue}
+        onChange={handleTextareaChange}
+      />
+
+      <span>Output: {output}</span>
+
+      <Button onClick={handleButtonClick}>Fetch</Button>
     </div>
   );
 }
